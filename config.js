bd0069323fb0d0ae66199675683d044753bc843d
heroz08 <zilongerdai@gmail.com>
2022-06-25 21:29:58 +0800

    修改提示

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export const homeServerUrl = 'http://im.ifelefi.com';
 
-export const accessToken = 'syt_d29ya2FudA_xqgCEZGeoIHiZHIDrZrD_4NMvB0';
+export const accessToken = 'syt_d29ya2FudA_iIVhQSwJoRwbPWydRmBy_2KWiaD';
--- modules/xianyu.js
+++ modules/xianyu.js
@@ -5,29 +5,30 @@
 import { saveFile, readFile, isExist } from '../utils/file.js';
 
 export default async function xianyu (...args) {
-    const [id, info = { a: 123 }] = args;
+    const [id, ...extArgs] = args;
+    const info = extArgs.join(' ')
     try {
         await isExist(import.meta.url, '../data/xy.json');
         const text = await readFile(import.meta.url, '../data/xy.json');
         const textInfo = JSON.parse(text.toString())
-        if(info){ // info存在 文件也存在
+        if(extArgs.length){ // info存在 文件也存在
             const isHas = textInfo.find(item => item.id === id)
             if(isHas) {
                 return '信息已经存在'
             } else {
-                textInfo.push(info)
+                textInfo.push({ id, info })
                 return save(textInfo)
             }
         } else {
             const isHas = textInfo.find(item => item.id === id)
             if(!isHas) {
-                return '信息已经不存在'
+                return '信息不存在'
             } else {
                 return isHas.info
             }
         }
     } catch (err) {
-        if (info) { // 文件名不存在 info存在
+        if (extArgs.length) { // 文件名不存在 info存在
             const obj = {
                 id, info
             };
