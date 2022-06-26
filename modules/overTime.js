b5ba9935cf63e5b5a3125fdb6a83ba97b04f445d
heroz08 <zilongerdai@gmail.com>
2022-06-26 15:49:32 +0800

    sdf

--- modules/overTime.js
+++ modules/overTime.js
@@ -5,21 +5,23 @@ import { now, isExist, saveFile, readFile } from '../utils/index.js';
 
 export default async function overtime () {
     const n = now();
+    const base = import.meta.url;
+    const dirPath = '../data/overtime.json';
     try {
-        await isExist(import.meta.url, '../data/overtime.json');
+        await isExist(base,);
         // 存在
-        const data = await readFile(import.meta.url, '../data/overtime.json');
+        const data = await readFile(base, dirPath);
         const oldData = JSON.parse(data.toString());
         oldData.push({ date: n, work: true });
 
         const buffer = Buffer.from(JSON.stringify(oldData));
-        await saveFile(buffer, import.meta.url, '../data/overtime.json');
+        await saveFile(buffer, base, dirPath);
         return '加班已经记录成功';
     } catch (e) {
         const obj = { date: n, work: true };
         const data = JSON.stringify([obj]);
         const buffer = Buffer.from(data);
-        await saveFile(buffer, import.meta.url, '../data/overtime.json');
+        await saveFile(buffer, base, dirPath);
         return '加班已经记录成功';
     }
 }
