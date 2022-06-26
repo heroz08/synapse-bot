0dc52269d3ae5dfd97b90e182708f2eb5b3d33d0
heroz08 <zilongerdai@gmail.com>
2022-06-26 15:51:10 +0800

    sdf

--- config.js
+++ config.js
@@ -3,3 +3,5 @@ export const homeServerUrl = 'http://im.ifelefi.com';
 export const accessToken = 'syt_d29ya2FudA_iIVhQSwJoRwbPWydRmBy_2KWiaD';
 
 export const roomId = '!neyrPDKxdgLtBLCHXQ:im.ifelefi.com';
+
+const a = 2123;
